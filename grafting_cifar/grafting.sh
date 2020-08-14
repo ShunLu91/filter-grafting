5e9f718a225d96d5a2e8f86e5085f455ade86902
lushun_mac <470651748@qq.com>
2020-08-15 00:29:23 +0800

    grafting_cifar10_mobilenetv2_4/1/2/3/4

--- grafting_cifar/grafting.sh
+++ grafting_cifar/grafting.sh
@@ -1,6 +1,13 @@
 #mkdir -pv checkpoint/grafting_cifar10_resnet32;
 #CUDA_VISIBLE_DEVICES=0 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 1 >checkpoint/grafting_cifar10_resnet32/1.log &
 #CUDA_VISIBLE_DEVICES=1 nohup python grafting.py --s checkpoint/grafting_cifar10_resnet32 --cifar 10  --model resnet32 --num 2 --i 2 >checkpoint/grafting_cifar10_resnet32/2.log &
-mkdir -pv checkpoint/grafting_cifar10_mobilenetv2;
-CUDA_VISIBLE_DEVICES=2 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 1 >checkpoint/grafting_cifar10_mobilenetv2/1.log &
-CUDA_VISIBLE_DEVICES=3 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 2 >checkpoint/grafting_cifar10_mobilenetv2/2.log &
+
+#mkdir -pv checkpoint/grafting_cifar10_mobilenetv2;
+#CUDA_VISIBLE_DEVICES=2 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 1 >checkpoint/grafting_cifar10_mobilenetv2/1.log &
+#CUDA_VISIBLE_DEVICES=3 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2 --cifar 10  --model mobilenetv2 --num 2 --i 2 >checkpoint/grafting_cifar10_mobilenetv2/2.log &
+
+mkdir -pv checkpoint/grafting_cifar10_mobilenetv2_4;
+CUDA_VISIBLE_DEVICES=0 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2_4 --cifar 10  --model mobilenetv2 --num 4 --i 1 >checkpoint/grafting_cifar10_mobilenetv2_4/1.log &
+CUDA_VISIBLE_DEVICES=1 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2_4 --cifar 10  --model mobilenetv2 --num 4 --i 2 >checkpoint/grafting_cifar10_mobilenetv2_4/2.log &
+CUDA_VISIBLE_DEVICES=2 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2_4 --cifar 10  --model mobilenetv2 --num 4 --i 3 >checkpoint/grafting_cifar10_mobilenetv2_4/3.log &
+CUDA_VISIBLE_DEVICES=3 nohup python grafting.py  --s checkpoint/grafting_cifar10_mobilenetv2_4 --cifar 10  --model mobilenetv2 --num 4 --i 4 >checkpoint/grafting_cifar10_mobilenetv2_4/4.log &
